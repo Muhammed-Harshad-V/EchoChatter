909d5fa04891ef6d09423dda7e0cbbba0519345a
Muhammed-Harshad-V <irshadiru472@gmail.com>
2025-02-01 17:38:50 +0530

    fix 10.1

--- frontend/src/Components/home/IndivitualChat.tsx
+++ frontend/src/Components/home/IndivitualChat.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPaperPlane, faImage } from "@fortawesome/free-solid-svg-icons";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import { FixedSizeList as List } from "react-window";
 import { useGlobalState } from "../../context/ContactsProvider";
 import { socketuri } from "../../api/api";
